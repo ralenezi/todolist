f69d758ee34f5313ecbb05b5ebc02595d0b24939
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-11-23 20:42:28 +0300

    backend updates ..

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,11 +1,22 @@
-import { makeAutoObservable } from 'mobx'
-import todos from '../todos'
+import { makeAutoObservable } from "mobx";
+// import todos from "../todos";
+import axios from "axios";
+
 class TodoStore {
-  todos = todos
+  todos = [];
 
   constructor() {
-    makeAutoObservable(this)
+    makeAutoObservable(this);
   }
+  fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/todos");
+      this.todos = response.data;
+    } catch (error) {
+      console.error("todoStore -> fetchTodos -> error", error);
+    }
+  };
 }
-const todoStore = new TodoStore()
-export default todoStore
+const todoStore = new TodoStore();
+todoStore.fetchTodos();
+export default todoStore;
