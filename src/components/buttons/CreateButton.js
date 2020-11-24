a547dbae61ffac5575e60ff5ff26ee9bc5310ab0
Rahaf Alenezi <ralenezi71@gmail.com>
2020-11-24 18:46:20 +0300

    done with adding - frontend

--- src/components/buttons/CreateButton.js
+++ src/components/buttons/CreateButton.js
@@ -1,11 +1,42 @@
-import React from "react";
-
+import React, { useState } from 'react'
+import todoStore from '../../stores/todoStore'
 const CreateButton = () => {
+  const [todo, setTodo] = useState({ name: '', status: '', priority: '' })
+  const handleChange = (e) => {
+    setTodo({ ...todo, [e.target.name]: e.target.value })
+  }
+  console.log('test ', todo)
+
+  const handleSubmit = (e) => {
+    // e.preventDefault()
+    todoStore.createTodo(todo)
+  }
   return (
     <div>
-      <button>Create Todo</button>
+      <input
+        name='name'
+        type='text'
+        placeholder='enter name of the todo'
+        onChange={handleChange}
+      />{' '}
+      <br />
+      <input
+        name='status'
+        type='text'
+        placeholder='enter status of the todo'
+        onChange={handleChange}
+      />{' '}
+      <br />
+      <input
+        name='priority'
+        type='text'
+        placeholder='enter priority of the todo'
+        onChange={handleChange}
+      />{' '}
+      <br />
+      <button onClick={handleSubmit}>Create Todo</button>
     </div>
-  );
-};
+  )
+}
 
-export default CreateButton;
+export default CreateButton
--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,37 +1,34 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable } from 'mobx'
 // import todos from "../todos";
-import axios from "axios";
-import slugify from "react-slugify";
+import axios from 'axios'
+import slugify from 'react-slugify'
 
 class TodoStore {
-  todos = [];
+  todos = []
 
   constructor() {
-    makeAutoObservable(this);
+    makeAutoObservable(this)
   }
   fetchTodos = async () => {
     try {
-      const response = await axios.get("http://localhost:8000/todos");
-      this.todos = response.data;
+      const response = await axios.get('http://localhost:8000/todos')
+      this.todos = response.data
     } catch (error) {
-      console.error("todoStore -> fetchTodos -> error", error);
+      console.error('todoStore -> fetchTodos -> error', error)
     }
-  };
+  }
   createTodo = async (newTodo) => {
-    newTodo.slug = slugify(newTodo.name);
-    newTodo.id = this.todos[this.todos.length - 1].id + 1;
-    this.todos.push(newTodo);
+    // newTodo.slug = slugify(newTodo.name)
+    // newTodo.id = this.todos[this.todos.length - 1].id + 1
+    // this.todos.push(newTodo)
     try {
-      const response = await axios.post(
-        "http://localhost:8000/candies",
-        newTodo
-      );
-      this.todos.push(response.data);
+      const response = await axios.post('http://localhost:8000/todos', newTodo)
+      this.todos.push(response.data)
     } catch (error) {
-      console.log("TodoStore -> createTodo -> error", error);
+      console.log('TodoStore -> createTodo -> error', error)
     }
-  };
+  }
 }
-const todoStore = new TodoStore();
-todoStore.fetchTodos();
-export default todoStore;
+const todoStore = new TodoStore()
+todoStore.fetchTodos()
+export default todoStore
