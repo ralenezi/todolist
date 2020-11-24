572cf2ffc7eb7db0963cb7bf1cecd9755502ce3a
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-11-24 17:16:54 +0300

    updates

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -1,6 +1,7 @@
 import { makeAutoObservable } from "mobx";
 // import todos from "../todos";
 import axios from "axios";
+import slugify from "react-slugify";
 
 class TodoStore {
   todos = [];
@@ -14,6 +15,20 @@ class TodoStore {
       this.todos = response.data;
     } catch (error) {
       console.error("todoStore -> fetchTodos -> error", error);
+    }
+  };
+  createTodo = async (newTodo) => {
+    newTodo.slug = slugify(newTodo.name);
+    newTodo.id = this.todos[this.todos.length - 1].id + 1;
+    this.todos.push(newTodo);
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/candies",
+        newTodo
+      );
+      this.todos.push(response.data);
+    } catch (error) {
+      console.log("TodoStore -> createTodo -> error", error);
     }
   };
 }
